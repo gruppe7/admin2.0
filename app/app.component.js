9f5281c4ba14ac96ef78bb612d5563154fcf38b2
eliassa <esundbya@gmail.com>
2016-09-22 12:43:06 +0200

    initial message

--- app/app.component.js
+++ app/app.component.js
@@ -15,7 +15,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>Angular App</h1>'
+            template: '<h1>Velkommen til Admin2.0</h1>'
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>Angular App</h1>'
+  template: '<h1>Velkommen til Admin2.0</h1>'
 })
 export class AppComponent { }
