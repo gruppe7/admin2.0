d430e83a249b5c3c69dcf2c6b566168b786eaea1
henrbj <henrik_bjorkheim@hotmail.com>
2016-10-20 13:13:46 +0200

    Changes to search

--- app/locker/mock-lockers.ts
+++ app/locker/mock-lockers.ts
@@ -1,7 +1,7 @@
 
 import { Locker } from './locker';
 export const LOCKERS: Locker[] = [
-  {id: 101, floor: 1},
-  {id: 102, floor: 1},
-  {id: 103, floor: 1}
+  {id: 101, floor: 1, taken: false},
+  {id: 102, floor: 1, taken: false},
+  {id: 103, floor: 1, taken: false}
 ];
