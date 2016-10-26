ec912b1e2f9e84047802abab87d0534a9a7f9391
eliassa <esundbya@gmail.com>
2016-10-26 15:48:10 +0200

    Login is working, but not logout! some confusing rxjs errors that keeps the app from deploying.

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,7 +4,7 @@
 **
 */
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 
 import { EventService } from './events/event.service';
@@ -15,7 +15,7 @@ import { UsersService } from './users/users.service';
 import { User } from './users/user';
 
 import { LoginComponent } from './login/login.component';
-import { AuthenticationService, AuthGuard } from './login/index';
+import { AuthenticationService, AuthGuard, AlertService} from './login/index';
 
 //fake backend
 import { fakeBackendProvider } from './login/fake-backend';
@@ -35,7 +35,12 @@ import { BaseRequestOptions } from '@angular/http';
       <a routerLink="/dashboard">Hjem</a>
       <a routerLink="/events">Events</a>
       <a routerLink="/lockers">Skap</a>
-      <a routerLink="/profile">Profil</a>
+      <a *ngIf="login" routerLink="/">Profil</a>
+
+      <a *ngIf="login" routerLink="/">Opprett Event</a>
+      <a *ngIf="login" routerLink="/">Administrer skap</a>
+      <a *ngIf="login" routerLink="/">Legg til bruker</a>
+
       <a *ngIf="!login" routerLink="/login">Logg inn</a>
       <a *ngIf="login" routerLink="/login">Logg ut</a>
     </nav>
@@ -51,6 +56,7 @@ import { BaseRequestOptions } from '@angular/http';
     UsersService,
     AuthenticationService,
     AuthGuard,
+    AlertService,
     User,
 
     // proveders for the fake backend:
@@ -59,12 +65,21 @@ import { BaseRequestOptions } from '@angular/http';
     BaseRequestOptions
             ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ADMIN 2.0';
+  login: boolean;
 
 
   constructor(){
 
+  }
+
+  ngOnInit(){
+    if(localStorage.getItem !== null){
+      this.login = true;
+    }else{
+      this.login = false;
+    }
   }
 
 
